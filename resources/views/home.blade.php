505b5175d1f759d425300cf02462f8532aedbe97
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-05-21 13:59:46 +0530

    Removing the cdns from home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,10 +63,22 @@
         }
 
     </style>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <link href="/MDB-Free/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="/MDB-Free/css/mdb.min.css" rel="stylesheet">
+
+{{--<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">--}}
+<!-- Scripts -->
+    <link href="/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">
+
+    <script type="text/javascript" src="/MDB-Free/js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="/MDB-Free/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="/MDB-Free/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="/MDB-Free/js/mdb.min.js"></script>
+
 </head>
 <body>
 <div class="flex-center position-ref full-height" style="background-color: #d7e8ff">
@@ -74,8 +86,8 @@
             @auth
                 <a href="{{ url('/home') }}" class="button">Home</a>
             @else
-                <a href="{{ url('/nurse/login') }}" > <button type="button" class="btn" style="border-radius: 50px; border-color: #98cbe8">Nurse Login</button></a>
-                <a href="{{ url('/admin/login') }}" > <button type="button" class="btn" style="border-radius: 50px; border-color: #98cbe8">Admin Login</button></a>
+                <a href="{{ url('/nurse/login') }}" > <button type="button" class="btn" style="border-radius: 50px; border-color: #98cbe8;color: black">Nurse Login</button></a>
+                <a href="{{ url('/admin/login') }}" > <button type="button" class="btn" style="border-radius: 50px; border-color: #98cbe8;color: black">Admin Login</button></a>
             @endauth
         </div>
          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
