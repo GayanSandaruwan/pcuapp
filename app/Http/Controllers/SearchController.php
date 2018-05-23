bd474697bbb67fcbd160b61015c3b882a91c96a4
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-05-23 18:53:18 +0530

    attempt top fix a bug in search for postgre

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -18,20 +18,20 @@ class SearchController extends Controller
             ->join('admissions','patients.id','=','admissions.patient_id')
             ->select('patients.*','admissions.admission_no','admissions.id as admission_id')
             ->where(DB::raw('LOWER(name)'),'LIKE','%'.strtolower($search).'%')
-            ->orderByRaw("CASE
+            ->orderByRaw("(CASE
                         WHEN LOWER(name) LIKE '".$search."%' THEN 1
                         WHEN LOWER(name) LIKE '%".$search."' THEN 3
                         ELSE 2
-                      END");
+                      END)");
         $patients_by_admission_no = DB::table('patients')
             ->join('admissions','patients.id','=','admissions.patient_id')
             ->select('patients.*','admissions.admission_no','admissions.id as admission_id')
             ->where(DB::raw('LOWER(admission_no)'),'LIKE','%'.strtolower($search).'%')
-            ->orderByRaw("CASE
-                        WHEN LOWER(name) LIKE '".$search."%' THEN 1
-                        WHEN LOWER(name) LIKE '%".$search."' THEN 3
+            ->orderByRaw("(CASE
+                        WHEN LOWER(admission_no) LIKE '".$search."%' THEN 1
+                        WHEN LOWER(admission_no) LIKE '%".$search."' THEN 3
                         ELSE 2
-                      END");
+                      END)");
 
         $patients = $patients_by_admission_no->union($patients_by_name)
                                                 ->get();
